39458e7f9f66b74097a95cfadfaa05addaa894bb
keksik31 <barabans44@gmail.com>
2026-05-23 10:42:21 +0700

    Refactor movement interface

--- BPO_PR1_zd3/Program.cs
+++ BPO_PR1_zd3/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Globalization;
-interface IMovable
+interface ISwimmable
 {
-    string Move();
+    string Swim();
 }
-abstract class Animal : IMovable
+
+abstract class Animal
 {
     public string Name { get; set; }
     public int Age { get; set; }
